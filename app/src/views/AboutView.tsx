ec17b508048b5bd24fc580f1435e544cceaad41b
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-01-17 20:09:15 +0000

    added small functionality

--- app/src/views/AboutView.tsx
+++ app/src/views/AboutView.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import logo from '../logo.svg'
 
 import { useHistory } from 'react-router-dom'
@@ -13,13 +13,26 @@ export default function AboutView() : JSX.Element{
         e.preventDefault()
         history.push("/")
     }
+    const [greeting,setGreeting] = useState("yahallo")
+    const changeGreeting = (e : React.MouseEvent) : void => {
+        e.preventDefault()
+        if (greeting === "yahallo"){
+            setGreeting("ohayo")
+        }else{
+            setGreeting("yahallo")
+        }
+    }
     return(
         <div className="App">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                this is the about page
-            </p>
-            <span onClick={goToHome}>click here to go home</span>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    this is the about page
+                </p>
+                <span onClick={goToHome}>click here to go home</span>
+                <span>{greeting}</span>
+                <span onClick={changeGreeting}>change greeting</span>                
+            </header>
         </div>
     )
 }
