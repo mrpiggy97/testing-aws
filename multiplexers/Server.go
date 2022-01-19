23f18fd0b6f40d646622560bffc83d29e4e769e3
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-01-19 14:14:59 +0000

    wrapper for httprouter

---
+++ multiplexers/Server.go
@@ -0,0 +1,37 @@
+package multiplexers
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type Server struct {
+	Router         *httprouter.Router
+	AllowedHost    string
+	AllowedMethods []string
+}
+
+func (serverInstance *Server) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if serverInstance.AllowedHost != "" {
+		if serverInstance.AllowedHost != request.Host {
+			fmt.Println("not allowed ", request.Host)
+			writer.WriteHeader(http.StatusForbidden)
+		}
+	}
+	var allowedMethods string = strings.Join(serverInstance.AllowedMethods, ",")
+	writer.Header().Add("Access-Control-Allow-Methods", allowedMethods)
+	serverInstance.Router.ServeHTTP(writer, request)
+}
+
+func NewServer() *Server {
+	var serverPointer *Server = &Server{
+		Router:         httprouter.New(),
+		AllowedHost:    os.Getenv("ALLOWED_HOST"),
+		AllowedMethods: []string{"GET"},
+	}
+	return serverPointer
+}
