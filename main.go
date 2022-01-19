569ccd88e67a69a2f585e45b4127c16d9dd61cfe
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-01-19 22:59:11 +0000

    simplified server

--- main.go
+++ main.go
@@ -3,20 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"time"
 
-	"github.com/julienschmidt/httprouter"
 	"github.com/mrpiggy97/testing-aws/multiplexers"
 )
 
 func main() {
-	var allowedHost string = os.Getenv("ALLOWED_HOST")
-	var multiplexer *multiplexers.Server = &multiplexers.Server{
-		Router:         httprouter.New(),
-		AllowedHost:    allowedHost,
-		AllowedMethods: []string{"GET"},
-	}
+	var multiplexer *multiplexers.Server = multiplexers.NewServer()
 	multiplexer.Router.ServeFiles("/*filepath", http.Dir("./app/build"))
 	var currentTime time.Time = time.Now()
 	var message string = fmt.Sprintf("server started listenting %v", currentTime)
