971c8f2a2ca1f41433a5b1cc14d44cdb9743c10f
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-01-19 14:14:45 +0000

    added a server wrapper for httprouter instance

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/mrpiggy97/testing-aws
 
 go 1.16
 
-require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
-)
+require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/mrpiggy97/testing-aws/multiplexers"
 )
 
 func main() {
-	var router *httprouter.Router = httprouter.New()
-	router.ServeFiles("/*filepath", http.Dir("./app/build"))
-	fmt.Println("listenting at port 5000")
-	http.ListenAndServe("0.0.0.0:5000", router)
+	var allowedHost string = os.Getenv("ALLOWED_HOST")
+	var multiplexer *multiplexers.Server = &multiplexers.Server{
+		Router:         httprouter.New(),
+		AllowedHost:    allowedHost,
+		AllowedMethods: []string{"GET"},
+	}
+	multiplexer.Router.ServeFiles("/*filepath", http.Dir("./app/build"))
+	var currentTime time.Time = time.Now()
+	var message string = fmt.Sprintf("server started listenting %v", currentTime)
+	fmt.Println(message)
+	http.ListenAndServe("0.0.0.0:5000", multiplexer)
 }
